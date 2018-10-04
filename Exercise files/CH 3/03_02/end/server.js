959251615c93eea5c4bd4530dc11210d7b580f52
Liran Harary <liran.harary@somoglobal.com>
2018-10-04 17:45:10 +0100

    Got to 2: Add Data...; Friends app: Created GraphQL server with a query and 2 mutations

---
+++ Exercise files/CH 3/03_02/end/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import path from 'path';
+import webpack from 'webpack';
+import WebPackDevServer from 'webpack-dev-server';
+import { schema } from './data/database'; // keep looking for this
+
+const APP_PORT = 3000;
+const GRAPHQL_PORT = 8080;
+
+// GraphQL server
+const graphQLServer = express();
+graphQLServer.use('/', graphqlHTTP({
+    schema: schema,
+    pretty: true,
+    graphiql: true,
+}));
+graphQLServer.listen(GRAPHQL_PORT, () => console.log(`GraphQL server on localhost:${GRAPHQL_PORT}`));
+
